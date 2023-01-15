a9877b2d8433b2c2948042fb838169540335d939
callme-devil <pransermi@gmail.com>
2023-01-15 14:13:08 +0330

    feat: Configured two page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
+const root = resolve(__dirname , 'src')
+const outDir = resolve(__dirname , 'dist')
+
 export default defineConfig({
-  root: 'src/Galaxy1/',
+  root,
   build: {
     // Relative to the root
-    outDir: '../dist',
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input:{
+        main: resolve(root ,'index.html'),
+        second: resolve(root ,'Galaxy2', 'index.html')
+      }
+    }
   },
   publicDir: '../static',
   plugins: [glsl()]
